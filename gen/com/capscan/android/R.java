7fb55fbe3e65222d1b93c4ccaffe03026b83efc8
Jeff Stephens <jefftheman45@gmail.com>
2012-03-04 03:06:05 -0600

    removed erroneous permission request

--- gen/com/capscan/android/R.java
+++ gen/com/capscan/android/R.java
@@ -16,6 +16,7 @@ public final class R {
     }
     public static final class id {
         public static final int capImgButton=0x7f050001;
+        public static final int imageView1=0x7f050004;
         public static final int imgview=0x7f050002;
         public static final int relativeLayout1=0x7f050000;
         public static final int takepic=0x7f050003;
