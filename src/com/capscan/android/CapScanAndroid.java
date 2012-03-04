3f30a7459006556f5cc77ea727b07c7f36bf1703
Andrew Hess <andrewclaytonhess@gmail.com>
2012-03-03 20:50:22 -0600

    made button, not quite working yet

--- src/com/capscan/android/CapScanAndroid.java
+++ src/com/capscan/android/CapScanAndroid.java
@@ -1,7 +1,11 @@
 package com.capscan.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class CapScanAndroid extends Activity {
 	/** Called when the activity is first created. */
@@ -9,5 +13,14 @@ public class CapScanAndroid extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		Button takePicButton = (Button) findViewById(R.id.takepic);
+		takePicButton.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				Intent captureIntent = new Intent("BottleCapCapture");
+				startActivity(captureIntent);
+			}
+		});
+
 	}
 }
