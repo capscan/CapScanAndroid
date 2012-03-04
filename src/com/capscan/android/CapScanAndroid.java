e54e6fee8f5e83395578017e154874ad5a5d5d35
Jeff Stephens <jefftheman45@gmail.com>
2012-03-03 19:11:37 -0600

    Added basic Eclipse project

---
+++ src/com/capscan/android/CapScanAndroid.java
@@ -0,0 +1,13 @@
+package com.capscan.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CapScanAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
