a3783930effef2ff05b3ec509e87a42c219fdb63
Jeff Stephens <jefftheman45@gmail.com>
2012-03-04 02:33:42 -0600

    Image capture kind of works. Stock Camera crashes in 2.2

--- gen/com/capscan/android/Manifest.java
+++ gen/com/capscan/android/Manifest.java
@@ -9,6 +9,7 @@ package com.capscan.android;
 
 public final class Manifest {
     public static final class permission {
+        public static final String ACCESS_FINE_LOCATION="android.permission.ACCESS_FINE_LOCATION";
         public static final String CAMERA="android.permission.CAMERA";
         public static final String autofocus="android.hardware.camera.autofocus";
         public static final String camera="android.hardware.camera";
--- src/com/capscan/android/CaptureImage.java
+++ src/com/capscan/android/CaptureImage.java
@@ -10,46 +10,59 @@ import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class CaptureImage extends Activity {
-	
+
 	ImageView img;
-	
+	private static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 555;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.imagecapture);
-		
+
 		img = new ImageView(this);
-		
-		if(isIntentAvailable(this, "MediaStore.ACTION_IMAGE_CAPTURE")) {
-			Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-			startActivityForResult(takePictureIntent, 1);
+
+		Button takePicButton = (Button) findViewById(R.id.capImgButton);
+		takePicButton.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
+				//					startActivityForResult(takePictureIntent, 1);
+				startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+			}
+		});
+
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			if(resultCode == RESULT_OK) {
+				Bundle extras = data.getExtras();
+				Bitmap mImageBitmap = (Bitmap) extras.get("data");
+				img.setImageBitmap(mImageBitmap);
+			}
+			else {
+				Toast.makeText(this, "Image Capture Failed", Toast.LENGTH_LONG).show();
+			}
 		}
 	}
-	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		handleSmallCameraPhoto(data);
-	}
-	
+
 	public static boolean isIntentAvailable(Context context, String action) {
-	    final PackageManager packageManager = context.getPackageManager();
-	    final Intent intent = new Intent(action);
-	    List<ResolveInfo> list =
-	            packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-	    return list.size() > 0;
+		final PackageManager packageManager = context.getPackageManager();
+		final Intent intent = new Intent(action);
+		List<ResolveInfo> list =
+				packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
 	}
-	
+
 	private void dispatchTakePictureIntent(int actionCode) {
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    startActivityForResult(takePictureIntent, actionCode);
-	}
-	
-	private void handleSmallCameraPhoto(Intent intent) {
-	    Bundle extras = intent.getExtras();
-	    Bitmap mImageBitmap = (Bitmap) extras.get("data");
-	    img.setImageBitmap(mImageBitmap);
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(takePictureIntent, actionCode);
 	}
 }
