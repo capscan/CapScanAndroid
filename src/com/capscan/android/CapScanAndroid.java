13aee639951da3e026ac69e915b6b5f860681481
Jeff Stephens <jefftheman45@gmail.com>
2012-03-03 19:39:22 -0600

    Added a Camera object

--- src/com/capscan/android/CapScanAndroid.java
+++ src/com/capscan/android/CapScanAndroid.java
@@ -1,13 +1,20 @@
 package com.capscan.android;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 
 public class CapScanAndroid extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Camera camera = Camera.open();
+		
+		if(camera == null) {
+			
+		}
+	}
 }
